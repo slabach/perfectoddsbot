2827952b56a65827a4ebc9f4517ae03e7c56e3b9
slabach <byron.slabach@gmail.com>
2026-03-12 09:07:15 -0400

    swap to gorm model

--- models/cardPlayHistory.go
+++ models/cardPlayHistory.go
@@ -1,26 +1,22 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type CardPlayHistory struct {
-	ID             uint           `gorm:"primaryKey"`
-	GuildID        string         `gorm:"index:idx_guild_target_created"`
-	TargetUserID   string         `gorm:"index:idx_guild_target_created"`
-	TargetUserDBID uint           `gorm:"index"`
-	CardID         uint           `gorm:"index"`
-	CardName       string         `gorm:"size:255"`
-	PlayedByUserID string         `gorm:"size:255"`
-	PointsBefore   float64        `gorm:"type:decimal(20,2)"`
-	PointsAfter    float64        `gorm:"type:decimal(20,2)"`
-	PointsDelta    float64        `gorm:"type:decimal(20,2)"`
-	HandCardsGained string        `gorm:"type:json"` // JSON array of card names
-	HandCardsLost   string        `gorm:"type:json"` // JSON array of card names
-	BetsResolved    string        `gorm:"type:json"` // JSON array of bet IDs
-	CreatedAt      time.Time      `gorm:"index:idx_guild_target_created"`
-	UpdatedAt      time.Time
-	DeletedAt      gorm.DeletedAt `gorm:"index"`
+	gorm.Model
+	ID              uint    `gorm:"primaryKey"`
+	GuildID         string  `gorm:"index:idx_guild_target_created"`
+	TargetUserID    string  `gorm:"index:idx_guild_target_created"`
+	TargetUserDBID  uint    `gorm:"index"`
+	CardID          uint    `gorm:"index"`
+	CardName        string  `gorm:"size:255"`
+	PlayedByUserID  string  `gorm:"size:255"`
+	PointsBefore    float64 `gorm:"type:decimal(20,2)"`
+	PointsAfter     float64 `gorm:"type:decimal(20,2)"`
+	PointsDelta     float64 `gorm:"type:decimal(20,2)"`
+	HandCardsGained string  `gorm:"type:json"` // JSON array of card names
+	HandCardsLost   string  `gorm:"type:json"` // JSON array of card names
+	BetsResolved    string  `gorm:"type:json"` // JSON array of bet IDs
 }
